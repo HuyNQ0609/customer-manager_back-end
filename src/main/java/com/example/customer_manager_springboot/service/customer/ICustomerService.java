9f35a4f4b5bf64611048bb8dbf4cc24e313641ac
HuyNQ0609 <nguyenhuy06092001@gmail.com>
2023-04-09 15:05:31 +0700

    Back end Customer manager

---
+++ src/main/java/com/example/customer_manager_springboot/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.example.customer_manager_springboot.service.customer;
+
+import com.example.customer_manager_springboot.model.Customer;
+import com.example.customer_manager_springboot.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
